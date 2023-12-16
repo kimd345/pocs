3ddd6cf959aade69268f6f245dd6ede6accffdf8
Dan Kim <kimd345@gmail.com>
2023-12-16 16:12:32 -0500

    Add Projectile object

--- games/script.js
+++ games/script.js
@@ -7,15 +7,43 @@ class Player {
 		this.y = this.game.height - this.height;
 		this.speed = 5;
 	}
+
 	draw(context) {
 		context.fillRect(this.x, this.y, this.width, this.height);
 	}
+
 	update() {
-		this.x += this.speed;
+		if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
+		if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
+
+		if (this.x < 0) this.x = 0;
+		else if (this.x > this.game.width - this.width)
+			this.x = this.game.width - this.width;
 	}
 }
 
-class Projectile {}
+class Projectile {
+	constructor() {
+		this.width = 4;
+		this.height = 20;
+		this.x = 0;
+		this.y = 0;
+		this.speed = 20;
+		this.free = true;
+	}
+	draw(context) {
+		if (!this.free) context.fillRect(this.x, this.y, this.width, this.height);
+	}
+	update() {
+		if (!this.free) this.y -= this.speed;
+	}
+	start() {
+		this.free = false;
+	}
+	reset() {
+		this.free = true;
+	}
+}
 
 class Enemy {}
 
@@ -24,7 +52,17 @@ class Game {
 		this.canvas = canvas;
 		this.width = this.canvas.width;
 		this.height = this.canvas.height;
+		this.keys = []; // keep track of keys that are pressed
 		this.player = new Player(this);
+
+		// event listeners
+		window.addEventListener('keydown', (e) => {
+			if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+		});
+		window.addEventListener('keyup', (e) => {
+			const index = this.keys.indexOf(e.key);
+			if (index > -1) this.keys.splice(index, 1);
+		});
 	}
 	render(context) {
 		this.player.draw(context);
@@ -32,9 +70,9 @@ class Game {
 	}
 }
 
-window.addEventListener("load", function () {
-	const canvas = document.getElementById("canvas1");
-	const ctx = canvas.getContext("2d");
+window.addEventListener('load', () => {
+	const canvas = document.getElementById('canvas1');
+	const ctx = canvas.getContext('2d');
 	canvas.width = 600;
 	canvas.height = 800;
 
